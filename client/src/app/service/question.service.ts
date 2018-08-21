4928c4cd6c65a8c37fe3b0e9ad6fb8bfcd149f63
Dai Wei <nbdavied@gmail.com>
2018-08-21 22:04:15 +0800

    test

--- client/src/app/service/question.service.ts
+++ client/src/app/service/question.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Question } from '../entity/question';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuestionService {
-
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getQuestion(method: string):Observable<Question>{
+    this.http.get(`/api/question?method=${method}`).pipe(
+      
+    );
+  }
 }
