7cb0f59a69264e5ff8b1f72729d39a3af9dad3f6
Dai Wei <nbdavied@gmail.com>
2018-09-18 16:37:17 +0800

    queslist 结构调整

--- client/src/app/module/paper/component/do-paper/do-paper.component.ts
+++ client/src/app/module/paper/component/do-paper/do-paper.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PaperService } from '../../service/paper.service';
+import { Question } from '../../../../entity/question';
+import { QuestionService } from '../../../../service/question.service';
 @Component({
   selector: 'app-do-paper',
   templateUrl: './do-paper.component.html',
@@ -8,12 +10,27 @@ import { PaperService } from '../../service/paper.service';
 })
 export class DoPaperComponent implements OnInit {
   paperId:number;
+  single:Question[];
+  multi:Question[];
+  truefalse:Question[];
   constructor(private route: ActivatedRoute,private paperService: PaperService,
-    private router: Router) {
+    private router: Router, private questionService:QuestionService) {
     this.paperId = +this.route.snapshot.paramMap.get('id');
    }
 
   ngOnInit() {
+    this.getQuestions();
+  }
+  getQuestions() {
+    this.questionService.getQuestionsInPaper(this.paperId, "s").subscribe(result => {
+      this.single = result;
+    });
+    this.questionService.getQuestionsInPaper(this.paperId, "m").subscribe(result => {
+      this.multi = result;
+    });
+    this.questionService.getQuestionsInPaper(this.paperId, "t").subscribe(result => {
+      this.truefalse = result;
+    })
   }
   finishPaper(){
     this.paperService.finishPaper(this.paperId).subscribe(
--- client/src/app/module/paper/component/ques-list/ques-list.component.ts
+++ client/src/app/module/paper/component/ques-list/ques-list.component.ts
@@ -3,34 +3,19 @@ import { Question } from '../../../../entity/question';
 import { QuestionService } from '../../../../service/question.service';
 import { PaperQuestion } from '../../entity/paper-question';
 import { PaperService } from '../../service/paper.service';
-import { Subject } from 'rxjs';
-import { debounce, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 @Component({
   selector: 'app-ques-list',
   templateUrl: './ques-list.component.html',
   styleUrls: ['./ques-list.component.css']
 })
 export class QuesListComponent implements OnInit {
-  @Input() type: string;
   @Input() paperId: number;
-  questions: Question[];
-  private selectTerm = new Subject<PaperQuestion>();
+  @Input() questions: Question[];
   constructor(private questionService: QuestionService, private paperService: PaperService) { }
 
   ngOnInit() {
-    this.getQuestions();
-    this.selectTerm.pipe(
-      //设置保存频率2秒
-      debounceTime(2000),
-      distinctUntilChanged(),
-      switchMap((q: PaperQuestion) => this.paperService.saveSelected(q))
-    ).subscribe();
   }
-  getQuestions(){
-    this.questionService.getQuestionsInPaper(this.paperId, this.type).subscribe(result =>{
-      this.questions = result;
-    })
-  }
+
   onSelectChange(questionId:number, selected:number[]){
     let q: PaperQuestion = new PaperQuestion();
     q.questionId = questionId;
